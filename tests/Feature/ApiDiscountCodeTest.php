95141a55c35478ac394147398a512e26ee4e8a93
kaerte <lucas.wski@gmail.com>
2021-04-28 23:12:44 +0200

    Fix tests

---
+++ tests/Feature/ApiDiscountCodeTest.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+
+namespace Ctrlc\DiscountCode\Tests\Feature;
+
+use Ctrlc\DiscountCode\Models\DiscountCode;
+use Ctrlc\DiscountCode\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ApiDiscountCodeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_api_valid_discount_code(): void
+    {
+        $discountCode = DiscountCode::factory()
+            ->active()
+            ->create();
+
+        $response = $this->postJson(route('api.discount-code.check'), [
+            'code' => $discountCode->code,
+        ]);
+
+        $response->assertJsonStructure([
+            'code',
+            'type',
+            'value',
+        ]);
+    }
+
+    public function test_api_disabled_discount_code(): void
+    {
+        $discountCode = DiscountCode::factory()
+            ->disabled()
+            ->create();
+
+        $response = $this->postJson(route('api.discount-code.check'), [
+            'code' => $discountCode->code,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonFragment([__('ctrlc_discount_code::messages.code_not_found')]);
+        $response->assertJsonStructure([
+            'errors' => [
+                0 => 'code',
+            ],
+        ]);
+    }
+
+    public function test_api_expired_discount_code(): void
+    {
+        $discountCode = DiscountCode::factory()
+            ->expired()
+            ->create();
+
+        $response = $this->postJson(route('api.discount-code.check'), [
+            'code' => $discountCode->code,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonFragment([__('ctrlc_discount_code::messages.code_expired')]);
+        $response->assertJsonStructure([
+            'errors' => [
+                0 => 'code',
+            ],
+        ]);
+    }
+}
---
+++ tests/Unit/DiscountCodeTest.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+namespace Ctrlc\DiscountCode\Tests\Unit;
+
+use Ctrlc\DiscountCode\Models\DiscountCode;
+use Ctrlc\DiscountCode\Tests\TestCase;
+
+class DiscountCodeTest extends TestCase
+{
+    public function test_factory(): void
+    {
+        $discountCode = DiscountCode::factory()->make();
+        self::assertInstanceOf(DiscountCode::class, $discountCode);
+    }
+
+    public function test_active(): void
+    {
+        $discountCode = DiscountCode::factory()
+            ->active()
+            ->make();
+
+        self::assertTrue($discountCode->isActive());
+    }
+
+    public function test_active_disabled(): void
+    {
+        $discountCode = DiscountCode::factory()
+            ->active()
+            ->disabled()
+            ->make();
+
+        self::assertNotTrue($discountCode->isActive());
+    }
+
+    public function test_enabled(): void
+    {
+        $discountCode = DiscountCode::factory()
+            ->enabled()
+            ->make();
+
+        self::assertTrue($discountCode->isActive());
+    }
+
+    public function test_disabled(): void
+    {
+        $discountCode = DiscountCode::factory()
+            ->disabled()
+            ->make();
+
+        self::assertNotTrue($discountCode->isActive());
+    }
+
+    public function test_expired(): void
+    {
+        $discountCode = DiscountCode::factory()
+            ->expired()
+            ->make();
+
+        self::assertNotTrue($discountCode->isActive());
+    }
+}
