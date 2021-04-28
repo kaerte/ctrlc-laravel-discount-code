f34413e142aa04ade328d271c303272d14e0c677
kaerte <lucas.wski@gmail.com>
2021-04-28 23:10:23 +0200

    Replace DiscountCodeType enum

---
+++ src/Enums/DiscountCodeTypeEnum.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Ctrlc\DiscountCode\Enums;
+
+use Spatie\Enum\Laravel\Enum;
+
+/**
+ * @method static self MONEY()
+ * @method static self PERCENT()
+ */
+class DiscountCodeTypeEnum extends Enum
+{
+}
