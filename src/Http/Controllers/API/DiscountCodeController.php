34d889c51514c3fdf3214012208672f872da749f
kaerte <lucas.wski@gmail.com>
2021-04-28 23:12:34 +0200

    Add DiscountCodeController.php

---
+++ src/Http/Controllers/API/DiscountCodeController.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Ctrlc\DiscountCode\Http\Controllers\API;
+
+use Ctrlc\DiscountCode\Models\DiscountCode;
+use Ctrlc\DiscountCode\Rules\DiscountCodeRule;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class DiscountCodeController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'code' => ['exists:discount_codes,code', new DiscountCodeRule],
+        ]);
+
+        return DiscountCode::where('code', $request->input('code'))->first();
+    }
+}
