3391f05437da0f400cf5fa03295ebcc0669f527c
kaerte <lucas.wski@gmail.com>
2021-04-28 23:12:19 +0200

    Load routes and translations

--- src/Providers/DiscountCodeServiceProvider.php
+++ src/Providers/DiscountCodeServiceProvider.php
@@ -16,5 +16,7 @@ class DiscountCodeServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations/2020_04_05_122340_create_discount_codes_table.php');
+        $this->loadRoutesFrom(dirname(__DIR__, 2).'/routes/api.php');
+        $this->loadTranslationsFrom(dirname(__DIR__, 2).'/resources/lang', 'ctrlc_discount_code');
     }
 }
