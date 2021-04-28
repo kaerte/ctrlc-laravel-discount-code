c3fb8ac6c8b7649f85e57f74f4448a05c63c9c12
kaerte <lucas.wski@gmail.com>
2021-04-28 23:10:47 +0200

    Fix scopes, set up model visible and casts

--- src/Models/DiscountCode.php
+++ src/Models/DiscountCode.php
@@ -2,8 +2,8 @@
 
 namespace Ctrlc\DiscountCode\Models;
 
-use Carbon\Carbon;
 use Ctrlc\DiscountCode\Database\Factories\DiscountCodeFactory;
+use Ctrlc\DiscountCode\Enums\DiscountCodeTypeEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,58 +13,60 @@ class DiscountCode extends Model
 {
     use HasFactory, SoftDeletes;
 
+    protected $visible = [
+        'code',
+        'type',
+        'value',
+    ];
+
     protected $dates = [
         'created_at',
         'updated_at',
-        'expires_at',
-        'starts_at',
         'deleted_at',
+        'active_from',
+        'active_to',
     ];
 
     protected $casts = [
-        'amount' => 'double',
-        'amount_type' => 'integer',
-        'active' => 'boolean',
+        'value' => 'integer',
+        'type' => DiscountCodeTypeEnum::class,
+        'code' => 'string',
     ];
 
-    const TYPE_PERCENTAGE = 0;
-    const TYPE_MONEY = 1;
-
-    //CUSTOM ATTRIBUTES
-    public function getFormattedAmountAttribute()
+    public function isActive()
     {
-        switch ($this->amount_type) {
-            case (self::TYPE_MONEY):
-                return currency_format((float) $this->amount, 'GBP');
-
-                break;
-            case (self::TYPE_PERCENTAGE):
-                return number_format($this->amount, 1) . '%';
-
-                break;
-        }
+        return
+            $this->enabled
+            && $this->active_from <= now()
+            && now() < $this->active_to;
     }
 
-    // SCOPES
+    public function isExpired()
+    {
+        return now() > $this->active_to;
+    }
+
+    public function scopeOfEnabled($query)
+    {
+        return $query->where('enabled', true);
+    }
+
+    public function scopeOfDisabled($query)
+    {
+        return $query->where('disabled', false);
+    }
+
+    public function scopeOfExpired($query)
+    {
+        return $query->where('active_to', '<=', now());
+    }
+
     public function scopeOfActive($query)
     {
-        return $query->where('active', 1);
-    }
-
-    // SCOPES
-    public function scopeOfInactive($query)
-    {
-        return $query->where('active', 0);
-    }
-
-    public function scopeOfNotExpired($query)
-    {
-        return  $query->where('expires_at', '>=', Carbon::now());
-    }
-
-    public function scopeOfStarted($query)
-    {
-        return  $query->where('starts_at', '<=', Carbon::now());
+        return $query
+            ->ofEnabled()
+            ->where('active_from', '<=', now())
+            ->where('active_to', '>', now());
     }
 
     protected static function newFactory(): DiscountCodeFactory
