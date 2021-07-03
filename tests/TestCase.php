bfa67abb4b8d8b6898c3d4efec3c8b70e5b71a7f
kaerte <lucas.wski@gmail.com>
2021-07-03 22:51:39 +0200

    Use loadLaravelMigrations in TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,18 +11,12 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function setUp(): void
     {
         parent::setUp();
+        $this->loadLaravelMigrations();
         $this->loadMigrationsFrom(__DIR__.'../database/migrations');
-        $this->loadMigrationsFrom(__DIR__.'../migrations');
     }
 
     protected function getPackageProviders($app)
     {
         return [DiscountCodeServiceProvider::class];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        include_once __DIR__.'/migrations/2014_10_12_000000_create_users_table.php';
-        (new \CreateUsersTable())->up();
     }
 }
