25d4ab640908c37be91fa8c6d3da2a2c4faf8d2e
kaerte <lucas.wski@gmail.com>
2021-07-04 14:09:49 +0200

    Rename migration file date

--- src/Providers/DiscountCodeServiceProvider.php
+++ src/Providers/DiscountCodeServiceProvider.php
@@ -10,7 +10,7 @@ class DiscountCodeServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations/2020_04_05_122340_create_discount_codes_table.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations/2020_01_01_000000_create_discount_codes_table.php');
         $this->loadTranslationsFrom(dirname(__DIR__, 2).'/resources/lang', 'ctrlc_discount_code');
     }
 }
