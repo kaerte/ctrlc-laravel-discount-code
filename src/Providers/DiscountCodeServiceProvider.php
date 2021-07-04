d7c24579c6ff8c2d064ea221f059f3f2721fc564
kaerte <lucas.wski@gmail.com>
2021-07-04 14:08:39 +0200

    Do not load routes in service provider

--- src/Providers/DiscountCodeServiceProvider.php
+++ src/Providers/DiscountCodeServiceProvider.php
@@ -8,15 +8,9 @@ use Illuminate\Support\ServiceProvider;
 
 class DiscountCodeServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        //
-    }
-
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations/2020_04_05_122340_create_discount_codes_table.php');
-        $this->loadRoutesFrom(dirname(__DIR__, 2).'/routes/api.php');
         $this->loadTranslationsFrom(dirname(__DIR__, 2).'/resources/lang', 'ctrlc_discount_code');
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Ctrlc\DiscountCode\Tests;
 
+use Ctrlc\DiscountCode\Http\Controllers\API\DiscountCodeController;
 use Ctrlc\DiscountCode\Providers\DiscountCodeServiceProvider;
+use Illuminate\Routing\Router;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
@@ -14,9 +16,15 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         $this->loadLaravelMigrations();
         $this->loadMigrationsFrom(__DIR__.'../database/migrations');
     }
-
+    
     protected function getPackageProviders($app)
     {
         return [DiscountCodeServiceProvider::class];
+    }
+
+    protected function defineRoutes($router): void
+    {
+        /** @var $router Router */
+        $router->post('/discount-code/check', DiscountCodeController::class)->name('api.discount-code.check');
     }
 }
